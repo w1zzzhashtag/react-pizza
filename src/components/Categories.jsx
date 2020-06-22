30145c4b97666fb8dde928e300678db2168062f6
Vladislav <wizardhype1998@gmail.com>
2020-06-22 23:04:39 +0400

    lesson 4

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,42 +1,23 @@
 import React, {useState} from 'react'
 
-// class Categories extends React.Component {
-//     state = {
-//         activeItem: 3,
-//     }
-
-//     onSelectItem(index) {
-//         this.setState({activeItem: index})
-//     }
-
-//     render() {
-//         const { items, onClickItem } = this.props
-//         return (
-//             <div className="categories">
-//                 <ul>
-//                     <li className="active">Все</li>
-//                     {items.map((item, index) => (
-//                         <li className={this.state.activeItem === index ? 'active' : ''} onClick={() => this.onSelectItem(index)} key={index}>{item}</li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         )
-//     }
-// }
-
 function Categories({items, onClickItem}) {
     const [activeItem, setActiveItem] = useState(null)
+
+    const onSelectItem = (index) => {
+        setActiveItem(index)
+    }
+
     return (
         <div className="categories">
             <ul>
                 <li 
-                    className={activeItem === null ? 'active': ''}
-                    onClick={() => setActiveItem(null)}
+                    className={activeItem === null ? 'active' : ''}
+                    onClick={() => onSelectItem(null)}
                 >Все</li>
                 {items && items.map((item, index) => (
                     <li 
                         className={activeItem === index ? 'active': ''}
-                        onClick={() => setActiveItem(index)} 
+                        onClick={() => onSelectItem(index)} 
                         key={index}
                     >
                         {item}
